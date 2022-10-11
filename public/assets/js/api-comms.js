91b986697e9a38a472e264ec7ada4837e2d15d54
RadioactiveDcay <33047670+RadioactiveDcay@users.noreply.github.com>
2022-10-11 16:36:33 -0700

    update: these are the edits made with TA last thursday. Just trying to manuever to another branch to start the SQL server

--- public/assets/js/api-comms.js
+++ public/assets/js/api-comms.js
@@ -3,20 +3,21 @@ const app = express();
 const port = 8080;
 const id = "LA, SD, SF, SAC, SJ";
 
-
-
-app.get('/city/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = re.body;
+app.get('/city/budget/:id/:min/:max', (req, res) => {
+    const { id,min,max } = req.params;
+    const { logo } = res.body;
 
     if (!logo) {
         res.status(418).send({ message: 'Please choose another option' })
     }
 
     res.send({
-        general_budget: {"Here is your expected salary ... " + (function {
-            TouchList: //Insert list information from 5 cities.
-        })}
+        general_budget: "Here is your expected salary ... "
     })
 
 });
+
+function grabCitybudgets() {
+    fetch('http://examples.com/movies.json')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
