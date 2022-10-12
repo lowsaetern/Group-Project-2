027f3965dae6e97eeb0438ea78d849207704eb96
Low Saetern <saetern.low123@yahoo.com>
2022-10-11 20:50:36 -0700

    starter database

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO Cities (city_name, food_cost, trans_cost, rent_cost, util_cost, entertain_cost, median_salary)
+VALUES
+('Los Angeles', '$602', '$256', '$2865', '$207', '$129', '$66,966'),
+('San Diego', '$596', '$176', '$2797', '$256', '$76', '$68,194'),
+('Sacramento', '$595', '$153', '$2380', '$327', '$71', '$65,525'),
+('San Jose', '$574', '$174', '$2722', '$283', '$106', '$86,460'),
+('San Francsico', '$693', '$172', '$3415', '$299', '$134', '$93,718');
