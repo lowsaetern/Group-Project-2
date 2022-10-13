42468c3a2d61368211a739ef5088d7de288cbf9a
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-10-13 14:53:37 -0700

    add heroku

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
+const { budgets } = require('./data/budgets.json');
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,7 +25,35 @@ const port = 8900;
 app.listen(port);
 console.log(`listening to server: http://localhost:${port}`);
 
+function filterByQuery(query, budgetsArray) {
+	let filteredResults = budgetsArray;
+	if (query.name) {
+		filteredResults = filteredResults.filter(
+			(budget) => budget.name === query.name
+		);
+	}
+	if (query.income) {
+		filteredResults = filteredResults.filter(
+			(budget) => budget.income === query.income
+		);
+	}
+	if (query.id) {
+		filteredResults = filteredResults.filter(
+			(budget) => budget.id === query.id
+		);
+	}
+	return filteredResults;
+}
+
 //landing page
 app.get('/', (req, res) => {
 	res.render('main');
+});
+
+app.get('/api/budgets', (req, res) => {
+	let results = budgets;
+	if (req.query) {
+		results = filterByQuery(req.query, results);
+	}
+	res.json(results);
 });
